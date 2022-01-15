a51a342eb3b6eb3718294434ef05a8e3405479d3
Matyi <a@a.a>
2022-01-15 07:35:05 +0100

    init

---
+++ src/main/java/hu/readysoft/nvsserver/NvsServerApplication.java
@@ -0,0 +1,13 @@
+package hu.readysoft.nvsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/readysoft/nvsserver/NvsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.readysoft.nvsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
