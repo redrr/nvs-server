4e90467a4bb4d9f875a8b2d80020270af5d7c0bc
Matyi <a@a.a>
2022-01-15 08:00:47 +0100

    init

--- src/main/java/hu/readysoft/nvsserver/statistics/StatisticsController.java
+++ src/main/java/hu/readysoft/nvsserver/statistics/StatisticsController.java
@@ -1,15 +1,29 @@
 package hu.readysoft.nvsserver.statistics;
 
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/statistics")
 public class StatisticsController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Hello";
+    public static final String BROWSER = "Chrome/79.0.3945.130";
+
+    @GetMapping("/all/solo")
+    public JSONObject test() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            Document doc = Jsoup.connect("https://www.valasztas.hu/ogy2018").userAgent(BROWSER).get();
+            jsonObject.put("size", doc.getAllElements().size());
+        } catch (Exception e) {
+            log.error("/statistics/test - parse error", e);
+        }
+        return jsonObject;
     }
 }
